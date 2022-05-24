b393b4646dba98da98730caf44366cf058588080
Chayan Vats <way2chayan11@gmail.com>
2022-05-24 22:32:54 +0530

    Linked-List

---
+++ Push_Front.cpp
@@ -0,0 +1,29 @@
+class Node {
+	int data;
+	Node* next;
+
+public:
+	Node(int d): data(d), next(NULL) {} //Initialisation List
+};
+
+class List {
+	Node* head;
+	Node* tail;
+public:
+	List(): head(NULL), tail(NULL) {}
+
+	void push_front(int data)
+	{
+		if (head == NULL)
+		{
+			Node* n = new Node(data);
+			head = tail = n;
+		}
+		else
+		{
+			Node* n = new Node(data);
+			n->next = head;
+			head = n;
+		}
+	}
+};
